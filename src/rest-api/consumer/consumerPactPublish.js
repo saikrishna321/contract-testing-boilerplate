a13acf96518afa699ea29c5de64a106c50a487ee
saikrishna321 <saikrishna321@yahoo.com>
2020-05-21 10:27:15 +0530

    publish pact to local pact broker

--- src/rest-api/consumer/consumerPactPublish.js
+++ src/rest-api/consumer/consumerPactPublish.js
@@ -1,26 +1,24 @@
-let pact = require("@pact-foundation/pact-node");
-const path = require("path");
+let pact = require('@pact-foundation/pact-node');
+const path = require('path');
 
 let opts = {
-  pactFilesOrDirs: [path.resolve(__dirname, "../../pacts/")],
-  pactBroker: "https://@@@@.pact.dius.com.au/",
+  pactFilesOrDirs: [path.resolve(__dirname, '../../../pacts/')],
+  pactBroker: 'http://localhost:8282/',
   pactBrokerUsername: process.env.PACT_USERNAME,
   pactBrokerPassword: process.env.PACT_PASSWORD,
-  consumerVersion: "1.0"
+  consumerVersion: '1.0',
 };
 
 pact
   .publishPacts(opts)
   .then(() => {
-    console.log("Pact contract publishing complete!");
-    console.log("");
-    console.log(
-      "Head over to https://@@@@.pact.dius.com.au/ and login with"
-    );
-    console.log("=> Username:", process.env.PACT_USERNAME);
-    console.log("=> Password:", process.env.PACT_PASSWORD);
-    console.log("to see your published contracts.");
+    console.log('Pact contract publishing complete!');
+    console.log('');
+    console.log('Head over to http://localhost:8282/ and login with');
+    console.log('=> Username:', process.env.PACT_USERNAME);
+    console.log('=> Password:', process.env.PACT_PASSWORD);
+    console.log('to see your published contracts.');
   })
-  .catch(e => {
-    console.log("Pact contract publishing failed: ", e);
+  .catch((e) => {
+    console.log('Pact contract publishing failed: ', e);
   });
